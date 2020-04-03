3447c6597dd7fac8a21ff226c4826b85ce07d88c
karapuce <typhaine.demangeon@gmail.com>
2020-04-03 11:09:14 +0200

    finalisation des requetes, une partie du css

--- public/book.php
+++ public/book.php
@@ -22,7 +22,7 @@ $query = "SELECT sum(payment) FROM bribe";
 $statement = $pdo->query($query);
 $total = $statement->fetch();;
 // je debug
-var_dump($total);
+//var_dump($total);
 ?>
 
 <main class="container">
@@ -37,14 +37,50 @@ var_dump($total);
             <div class="page leftpage">
                 Add a bribe
                 <!-- TODO : Form -->
+                <?php
+                //verification si le form a déjà été soumis
+                if(isset($_POST['added'])) {
+                    //verification du form
+                    if ((!empty($_POST['name'])) and (!empty($_POST['payment']))) {
+                        // verif paiement supérieur à 0
+                        if ($_POST['payment'] <= 0) {
+                            echo "<h2>Il y a une erreur dans ce montant, corrigez-le avant de valider!</h2>";
+                        } // si tout est ok
+                        else {
+                            // requete d'envoi
+                            $query = "INSERT INTO bribe (name,payment) VALUES (:name,:payment)";
+                            $name = trim($_POST['name']);
+                            $payment = trim($_POST['payment']);
+
+                            $statement = $pdo->prepare($query);
+                            $statement->bindValue(":name", $name, PDO::PARAM_STR);
+                            $statement->bindValue(":payment", $payment, PDO::PARAM_STR);
+                            $statement->execute();
+
+                            header("location: book.php");
+                        }
+                    } else {
+                        echo "<h2>Aucun champ ne doit être vide</h2>";
+                    }
+                }
+                ?>
+                <form method="post">
+                    <label for="name">Name</label>
+                    <input type="text" id="name" name="name">
+                    <label for="payment">Payment</label>
+                    <input type="number" id="payment" name="payment">
+                    <input type="submit" name="added" value="Pay!">
+                </form>
             </div>
 
             <div class="page rightpage">
                 <!-- TODO : Display bribes and total paiement -->
                 <table>
                     <thead>
-                    <th>S</th>
+                    <th colspan="2">S</th>
+
                     </thead>
+
                     <tbody>
                     <?php
                     //affichage de la liste
