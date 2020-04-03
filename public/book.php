b2e8fef4bba5332d24019a5177bfa3d3ccab227c
Varkoff <virgileboris96@hotmail.fr>
2020-04-03 10:25:54 +0200

    initial commit + css

--- public/book.php
+++ public/book.php
@@ -14,20 +14,20 @@
 <main class="container">
 
     <section class="desktop">
-        <img src="image/whisky.png" alt="a whisky glass" class="whisky"/>
-        <img src="image/empty_whisky.png" alt="an empty whisky glass" class="empty-whisky"/>
-
         <div class="pages">
-            <div class="page leftpage">
+            <img src="image/whisky.png" alt="a whisky glass" class="whisky"/>
+            <img src="image/empty_whisky.png" alt="an empty whisky glass" class="empty-whisky"/>
+            <div class="pageLeftpage">
                 Add a bribe
                 <!-- TODO : Form -->
             </div>
 
-            <div class="page rightpage">
+            <div class="pageRightpage">
                 <!-- TODO : Display bribes and total paiement -->
             </div>
+            <img src="image/inkpen.png" alt="an ink pen" class="inkpen"/>
         </div>
-        <img src="image/inkpen.png" alt="an ink pen" class="inkpen"/>
+
     </section>
 </main>
 </body>
