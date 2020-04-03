34bc98e023c62458519ddef7f097e5717437005a
Varkoff <virgileboris96@hotmail.fr>
2020-04-03 12:34:01 +0200

    last changes + bonus

--- public/book.php
+++ public/book.php
@@ -17,14 +17,30 @@ $paymentList = new PDO(DSN, USER, PASS);
 $query = 'SELECT name, payment from bribe';
 $total = 'SELECT SUM(payment) from bribe';
 
+$alphabet = array_merge(range('A', 'Z'));
+
+$error = false;
 
 if (!empty($_POST['name']) && !empty($_POST['payment'])) {
-    $insertQuery = 'INSERT INTO bribe (name, payment) VALUES (:name, :payment)';
-    $addPayment = $paymentList->prepare($insertQuery);
 
-    $addPayment->bindValue(":name", $_POST['name']);
-    $addPayment->bindValue(":payment", $_POST['payment']);
-    $addPayment->execute();
+    if ($_POST['name'] != "Eliott Ness"){
+        $insertQuery = 'INSERT INTO bribe (name, payment) VALUES (:name, :payment)';
+        $addPayment = $paymentList->prepare($insertQuery);
+
+        $addPayment->bindValue(":name", $_POST['name']);
+        $addPayment->bindValue(":payment", $_POST['payment']);
+        $addPayment->execute();
+    }else{
+        $error=true;
+    }
+}
+
+if (!empty($_GET['firstLetter'])) {
+    $query = 'SELECT name, payment from bribe WHERE name LIKE' . '\'' . $_GET['firstLetter'] . '%\'';
+    $total = 'SELECT SUM(payment) from bribe WHERE name LIKE' . '\'' . $_GET['firstLetter'] . '%\'';
+    $title = $_GET['firstLetter'];
+} else {
+    $title = "Total";
 }
 
 ?>
@@ -32,6 +48,16 @@ if (!empty($_POST['name']) && !empty($_POST['payment'])) {
 <main class="container">
 
     <section class="desktop">
+        <div class="index">
+            <form action="book.php" method="get">
+
+                <?php
+                foreach ($alphabet as $value) {
+                    echo "<input type=\"submit\" name=\"firstLetter\" value=\"$value\">";
+                }
+                ?>
+            </form>
+        </div>
         <div class="pages">
             <img src="image/whisky.png" alt="a whisky glass" class="whisky"/>
             <img src="image/empty_whisky.png" alt="an empty whisky glass" class="empty-whisky"/>
@@ -44,14 +70,23 @@ if (!empty($_POST['name']) && !empty($_POST['payment'])) {
                     <label for="payment">Payment</label>
                     <input type="number" min="1" name="payment" required>
                     <input type="submit" value="Pay !" class="submit">
-
                 </form>
+                <?php
+                if ($error){
+                    echo "<h3>This man is untouchable</h3>";
+                }
+                ?>
             </div>
 
             <div class="pageRightpage">
                 <!-- TODO : Display bribes and total paiement -->
                 <table>
                     <thead>
+                    <tr>
+                        <?php
+                        echo "<div class='title'>$title</div><hr>";
+                        ?>
+                    </tr>
                     <tr>
                         <th>Name</th>
                         <th>Payment</th>
@@ -86,3 +121,4 @@ if (!empty($_POST['name']) && !empty($_POST['payment'])) {
 </main>
 </body>
 </html>
+<?php
