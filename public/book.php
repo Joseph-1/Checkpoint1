dfa6a78d5bbf9b753c88faf39ee5333d57c1b11d
Vincent <fritsch_vincent@hotmail.fr>
2020-04-03 12:57:11 +0200

    Finished section 3 but couldn't fix css

--- public/book.php
+++ public/book.php
@@ -1,5 +1,7 @@
 <?php 
 require('connec.php');
+$name = $_POST['name'];
+$payment=$_POST['payment'];
 ?>
 <!doctype html>
 <html lang="en">
@@ -24,13 +26,51 @@ require('connec.php');
         <div class="pages">
             <div class="page leftpage">
                 Add a bribe
+                <br>
+              
                 <!-- TODO : Form -->
+                <?php   if($name != NULL && $payment>0) 
+                        {$query = "INSERT INTO bride (bride_name, payment) VALUES (:bride_name, :payment)";
+                            $statement = $pdo->prepare($query);
+                            $statement->bindValue(':bride_name',$name, \PDO::PARAM_STR);
+                            $statement->bindValue(':payment', $payment, \PDO::PARAM_INT);
+                                
+                            $statement->execute();
+                            echo "You're welcome !!";
+                        }
+                        elseif( $payment<0)
+                        {
+                            echo "The payment can't and couldn't be negative";
+                        }
+                        else
+                        {
+                            echo "You should complet the form !!!";
+                        }
+                        ?>
+                
+                <form action="" method="post" class="form-example">
+                    <div class="form-example">
+                        <label for="name">Name 
+                        <input type="text" name="name" id="name" >
+                        </label>
+                    </div>
+                    <div class="form-example">
+                        <label for="payment">Payment 
+                        <input type="number" name="payment" id="payment" >
+                        </label>
+                    </div>
+                    <div class="form-example">
+                        <input type="submit" value="Pay !">
+                    </div>
+                </form>
+                <?php $name= $_POST["name"];
+                    $payment=$_POST['payment'];?>
             </div>
 
             <div class="page rightpage">
                 <!-- TODO : Display bribes and total paiement -->
                 <?php
-                $query = "SELECT * FROM bride ORDER BY name ASC";
+                $query = "SELECT * FROM bride ORDER BY bride_name ASC";
                 $statement = $pdo->query($query);
                 $brides= $statement->fetchAll();
                 ?>
@@ -45,14 +85,14 @@ require('connec.php');
                     <?php foreach($brides as $bride)
                         {?>
                         <tr>
-                        <td><?php echo " $bride[name]";?></td>
+                        <td><?php echo " $bride[bride_name]";?></td>
                         <td class="left"><?php echo " $bride[payment]";?></td>
                         </tr>
                         <?php }?>
                     </tbody>
                     <tfoot>
                         <tr>
-                        <td></td>
+                        <td>Total</td>
                         <?php $query = "SELECT SUM(payment) FROM bride";
                                 $statement = $pdo->query($query);
                                 $sum= $statement->fetch();
@@ -61,6 +101,7 @@ require('connec.php');
                         </tr>
                        
                     </tfoot>
+                </table>
             </div>
         </div>
         
